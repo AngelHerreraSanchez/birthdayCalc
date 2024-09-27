14b47e4e525e78c78101d2a139ebbb845f54fac4
herreang000 <herreang000@warren.k12.in.us>
2024-09-27 11:54:24 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LocalDateTime birthday = LocalDateTime.of(2007, 3, 29, 5, 0, 0);
+        ZoneId us = ZoneId.of("America/New_York");
+        ZonedDateTime birthdayZoned = ZonedDateTime.of(birthday, us);
+
+        ZoneId london = ZoneId.of("Europe/Paris");
+        ZonedDateTime birthdayParis = birthdayZoned.withZoneSameInstant(london);
+        System.out.println(birthdayZoned);
+        System.out.println(birthdayParis);
+
+
+
+
+    }
 }
